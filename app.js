94ebf22e0da7b768e9c9b03ca57d5a0d53bd340c
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-10-27 17:20:51 +0100

    update

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-
 const express = require("express");
-const app = express();
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const router = express.Router();
+const app = express();
 
-router.get("/home", (req, res) => {
-  res.send("Hello World, This is home router");
+app.use(session({ secret: "ssshhhhh", saveUninitialized: true, resave: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var sess; // global session, NOT recommended, only for demonstration purposes
+
+router.get("/", (req, res) => {
+  sess = req.session;
+  if (sess.email) {
+    return res.redirect("/admin");
+  }
+  res.send("Ok");
 });
 
-router.get("/profile", (req, res) => {
-  res.send("Hello World, This is profile router");
+router.post("/login", (req, res) => {
+  sess = req.session;
+  sess.email = req.body.email;
+  res.end("done");
 });
 
-router.get("/login", (req, res) => {
-  res.send("Hello World, This is login router");
+router.get("/admin", (req, res) => {
+  sess = req.session;
+  if (sess.email) {
+    res.write(`Hello ${sess.email}`);
+    res.end("Logout");
+  } else {
+    res.write("Please login first.");
+    res.end("Login");
+  }
 });
 
 router.get("/logout", (req, res) => {
-  res.send("Hello World, This is logout router");
+  req.session.destroy((err) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect("/");
+  });
 });
 
 app.use("/", router);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Web Server is listening at port " + (process.env.PORT || 3000));
+  console.log(`App Started on PORT ${process.env.PORT || 3000}`);
 });
